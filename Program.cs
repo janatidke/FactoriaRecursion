e4e44ad6d128b39811094e14aafdd122b3cee2d8
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-09-01 20:56:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FactoriaRecursion
+{
+    class Fact
+    {
+        internal int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Factorial of given number is: " + new Fact().Factorial(n));
+        }
+    }
+}
